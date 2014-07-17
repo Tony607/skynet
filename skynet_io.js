9726bc4be2c69507d418fec536075764a1c46742
chengwei.zhang <chengwei.zhang@intelligrated.com>
2014-07-17 15:07:22 -0400

    first commit

--- skynet_io.js
+++ skynet_io.js
@@ -8,14 +8,14 @@ socket = io.connect('http://skynet.im', {
 
 socket.on('connect', function () {
 	console.log('Requesting websocket connection to SkyNet');
-	// Replace 623dd....112c with your uuid and replace the token as well.
+	// Replace d4bc3851....4f9 with your uuid and replace the token as well.
 	socket.on('identify', function (data) {
 		console.log('Websocket connecting to SkyNet with socket id: ' + data.socketid);
 		console.log('Sending device uuid: d4bc3851-0d2a-11e4-b13f-933e845654f9');
 		socket.emit('identity', {
-			uuid : 'd4bc3851-0d2a-11e4-b13f-933e845654f9',
+			uuid : 'd4bc3851-0d2a-11e4-b13f-933e845654f9',//the local device UUID 
 			socketid : data.socketid,
-			token : '5ira2ftm2mb49529miqzyk25z7kz9f6r'
+			token : '5ira2ftm2mb49529miqzyk25z7kz9f6r'//the local device token
 		});
 	});
 
@@ -34,7 +34,7 @@ socket.on('connect', function () {
 			socket.on('message', function (msg) {
 				//       console.log('message received', msg);
 				socket.emit("message", {
-					"devices" : '449cb1c1-0de1-11e4-ba98-ed547cf24cbd',
+					"devices" : '449cb1c1-0de1-11e4-ba98-ed547cf24cbd',//the remote device UUID
 					"payload" : {
 						"serialin" : "Got_It--->NewValue<"+parseInt(Math.random()*100)
 					}
